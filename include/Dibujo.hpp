d4b760628d80645b848adbf7bcdf5970b969460c
aldozavala <160289969+aldozavala@users.noreply.github.com>
2024-04-23 01:46:04 +0000

    Version 2

---
+++ include/Dibujo.hpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <string>
+#include <ftxui/dom/elements.hpp>
+using namespace std;
+using namespace ftxui;
+class Dibujo
+{
+private:
+    ifstream archivo;
+    Element contenido;
+
+public:
+    Dibujo(string path)
+    {
+        archivo.open(path);
+            string linea;
+        Elements elementos;
+        while (getline(archivo, linea))
+        {
+            elementos.emplace_back(text(linea));
+        }
+     contenido = vbox(elementos);
+        archivo.close();
+    }
+
+    Element GetElement()
+    {
+        return this->contenido;
+    }
+    ~Dibujo() {}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,22 @@
 #include<thread>//hilos de proceso
 #include<fstream>//flujos de archivos
 #include<string>//control de cadenas
+#include<Dibujo.hpp>
 using namespace std;
 using namespace ftxui;
 
     int main (int argc, char const *argv[])
     {
-        fstream archivo;
-        archivo.open("assets/assets/images/moto.txt");
-        string moto;
-        archivo >> moto;
-        while (archivo.good())
-        {
-            archivo >> moto;
-        }
-        archivo.close();
-        this_thread::sleep_for(0.1s);
+        Dibujo moto("./assets/assets/images/moto.txt");
+        Dibujo bloque("./assets/assets/images/bloque.txt");
         int fotograma=0;
         while(true)
         {
+        this_thread::sleep_for(0.1s);
         fotograma ++;
         Element personaje = spinner (21,fotograma) | bold | color(Color::Red) | bgcolor(Color::White);
-        Element eMoto = text(moto)| bold | color(Color::Orange1) | bgcolor(Color::White);;
-        Element dibujo = hbox({personaje,eMoto});
+       // Element eMoto = moto| bold | color(Color::Orange1) | bgcolor(Color::White);;
+        Element dibujo = vbox({personaje,moto.GetElement(),bloque.GetElement()});
         Screen pantalla = Screen::Create(Dimension::Full());
         Render(pantalla,dibujo);
         pantalla.Print();
